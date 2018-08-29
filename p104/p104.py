3c642ca94f5022bf09581ff28d8c9b260b87ee0b
fortran <alejandro.carrasco@bazaarvoice.com>
2018-08-29 03:12:38 +0200

    p104: Arbitrage
    my intuition was more or less on track, but I had to check a hint
    to actually solve this :(
    
    it is a Floyd-Warshall algorithm with an extra dimension to keep
    the best solution so far of size at most s in each step.

--- p104/p104.py
+++ p104/p104.py
@@ -12,18 +12,6 @@ def add_diagonal(wod):
 			wd[i][wdj] = v
 	return wd
 
-def fmt(solution):
-	for i in solution:
-		print(i)
-	print()
-
-def diagonal(fx_sequences):
-	return [row[i] for i, row in enumerate(fx_sequences)]
-
-def profitable_fx_sequence(rates, fx_sequences):
-	profitable_sequences = [fxs for fxs in diagonal(fx_sequences) if fx_sequence_value(rates, fxs) > 1.01]
-	return min(profitable_sequences, key=len, default=None)
-
 def flatten(table):
 	return chain(*table)
 
@@ -33,53 +21,36 @@ def pairs(fx_sequence):
 def ops_to_fx_sequence(pairs):
 	return list(flatten([p[0:-1] for p in pairs])) + [pairs[-1][-1]]
 
-def min_fx_sequence_length(fx_sequences):
-	return len(min(flatten(fx_sequences), key=len))
-
 def fx_sequence_value(rates, fx_sequence):
 	value = 1
 	for i, j in pairs(fx_sequence):
 		value *= rates[i][j]
 	return value
 
-def optimize_fx_sequence(fx_sequence, fx_sequences):
-	def expand():
-		for i, j in pairs(fx_sequence):
-			yield fx_sequences[i][j]
-	return ops_to_fx_sequence(list(expand()))
-
-def generate_fx_sequences_through(i, j, k, fx_sequences):
-	ij_fx_sequence = fx_sequences[i][j]
-	yield fx_sequences[i][j]
-	for n in range(1, len(ij_fx_sequence)):
-		through_k = ij_fx_sequence[:n] + [k] + ij_fx_sequence[n:]
-		yield optimize_fx_sequence(through_k, fx_sequences)
-		
-def arbitrage(rates):
-	n = len(rates)
-	def shorter_than_n(fx_sequence):
-		return len(fx_sequence) <= n
-		
-	fx_sequences = [[[i, j] for j in range(n)] for i in range(n)]
-	value = partial(fx_sequence_value, rates)
-	old_fx_sequences = None
-	while (old_fx_sequences != fx_sequences and
-	       not profitable_fx_sequence(rates, fx_sequences)):
-		old_fx_sequences = deepcopy(fx_sequences)
-		#print(old_fx_sequences)
-		for i in range(n):
-			for j in range(n):
-				for k in range(n):
-					candidates = list(filter(shorter_than_n, generate_fx_sequences_through(i, j, k, old_fx_sequences)))
-					#print('i=%s j=%s k=%s candidates=%s' % (i, j, k, [(x, value(x)) for x in candidates]))
-					best = max(candidates, key=lambda x:(value(x), -len(x)))
-					#print('best=%s' % (best,))
-					fx_sequences[i][j] = max(fx_sequences[i][j], best, key=lambda x:(value(x), -len(x)))
-	v = profitable_fx_sequence(rates, fx_sequences)
+def fmt(v):
 	if v:
 		return ' '.join(str(x+1) for x in v)
 	else:
 		return 'no arbitrage sequence exists'
+
+def arbitrage(rates):
+	n = len(rates)
+	fx = [[{1: (rates[i][j], [i, j])} for j in range(n)] for i in range(n)]
+	def value(x):
+		return x[0]
+	def concat(ij, k):
+		ijv, ijs = ij
+		kv, ks = k
+		return (ijv * kv, ijs[:-1] + ks)
+	for s in range(2, n+1):
+		for i in range(n):
+			for j in range(n):
+				fx[i][j][s] = fx[i][j][s-1]
+				for k in range(n):
+					fx[i][j][s] = max(fx[i][j][s], concat(fx[i][k][s-1], fx[k][j][1]), key=value)
+					if i == j and value(fx[i][j][s]) > 1.01:
+						return fx[i][j][s][1]
+	return None
 	
 
 lines = sys.stdin.readlines()
@@ -88,5 +59,4 @@ while lines:
 	without_diagonal = [[float(x) for x in line.split()] for line in lines[:n]]
 	lines[:n] = []
 	rates = add_diagonal(without_diagonal)
-	#print(rates)
-	print(arbitrage(rates))
+	print(fmt(arbitrage(rates)))
